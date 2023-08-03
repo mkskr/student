0afa936e7b4267ff566624478d498aa05f8ea674
mukesh <mkshsingh976@gmail.com>
2023-08-03 17:48:13 +0530

    logger msg added in main

--- src/main/java/com/mks/student/StudentApplication.java
+++ src/main/java/com/mks/student/StudentApplication.java
@@ -1,13 +1,18 @@
 package com.mks.student;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentApplication {
+	private static Logger logger = LoggerFactory.getLogger(StudentApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("---------before main start-----------");
 		SpringApplication.run(StudentApplication.class, args);
+		logger.info("--------after main end----------------");
 	}
 
 }
